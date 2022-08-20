39b6d43194117756ce46c1dc4ffc418e2f27082f
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-08-20 18:32:33 -0300

    Initial commit

---
+++ organograma/src/componentes/CampoTexto/index.js
@@ -0,0 +1,14 @@
+
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input/>
+        </div>
+    )
+
+
+}
+
+export default CampoTexto;
