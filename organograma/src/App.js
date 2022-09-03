800230d67c695baa1d976bc0961f5ac5cfbcadd6
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-09-03 17:50:07 -0300

    criando os times e as cores

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -1,9 +1,43 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+  },
+  {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+  },
+  {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+  },
+  {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE95F5',
+  },
+  {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+  },
+  {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+  }
+  ]
 
 const [colaboradores, setColaboradores] = useState([])
 
@@ -17,7 +51,10 @@ const aoNovoColaboradorAdicionado = (colaborador) => {
 
       <Banner />
     <Formulario aoColaboradorCadastrdo={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-    
+    <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+        
+
+
     </div>
   );
 }
